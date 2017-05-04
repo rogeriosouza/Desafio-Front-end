58ae7153866ec87ecd8eefb6c474889b75f4a77c
Rogerio Souza <rogerio.fsouza2@gmail.com  git config --global user.name Rogerio>
2017-05-04 14:33:48 -0300

    1 commmit

---
+++ Desafio-vue/src/App.vue
@@ -0,0 +1,52 @@
+<template>  
+  <div id="app">
+    <nav class="nav">
+            <div class="nav-left">
+                {{name}}
+            </div>
+            <span class="nav-item">
+                <a v-link="{ path: '/' }" class="button is-primary">
+                    <span class="icon">
+                    <i class="fa fa-home"></i>
+                    </span>
+                    <span>Main Content</span>
+                </a>
+                <a v-link="{ path: '/resourceExample' }" class="button is-primary">
+                    <span class="icon">
+                    <i class="fa fa-link"></i>
+                    </span>
+                    <span>Resource example</span>
+                </a>
+                <a v-link="{ path: '/vuexExample' }" class="button is-primary">
+                    <span class="icon">
+                    <i class="fa fa-bolt"></i>
+                    </span>
+                    <span>Vuex example</span>
+                </a>
+            </span>
+        </nav>
+    <div>
+        <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import store from './vuex/store'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue!'
+    }
+  },
+  store
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ Desafio-vue/src/content/ResourceContent.vue
@@ -0,0 +1,31 @@
+<template>  
+    Resuorce Content
+
+    <button @click="onButtonClick">get foo.json</button> 
+    \{{fooMessage}}
+
+</template>  
+<script>  
+    export default{
+        data () {
+            return {
+                fooMessage: ''
+            }
+        },
+        methods :{
+            onButtonClick() {
+                var foo = this;
+                this.$http.get('/foo.json').then(
+                (response) => {
+                    //console.log('TESTE: '+response.data.msg);
+                    //Usando JSON não funcionou, li direto do DATA.
+                    //foo.fooMessage = JSON.parse(response.data).msg
+                    foo.fooMessage = response.data.msg
+                },
+                (error) => {
+                     console.error(error.statusText)
+                })
+            }
+        }
+    }    
+</script>
